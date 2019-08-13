b5227a91b505fab2dc2ee74220849e68bd66870c
kenny bergounhe <bergounhe.kenny.code@gmail.com>
2019-08-13 17:31:32 +0800

    routes done and confirmed by Guido

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/dashboard', to: 'pages#dashboard', as: 'user_dashboard'
+  resources :photos, except: [:edit, :update] do
+    resources :products, only: [:create]
+  end
+  resources :cart_products, only: [:destroy]
+  resources :carts, only: [:show] do
+    resources :orders, only: [:create]
+  end
+  resources :orders, only: [:show, :edit, :update]
 end
