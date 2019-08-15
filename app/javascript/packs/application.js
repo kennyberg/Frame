41aab99d663b0dc17f0941782f6d4f8d947fcc3a
kenny bergounhe <bergounhe.kenny.code@gmail.com>
2019-08-15 12:59:23 +0800

    commented all the js codes to make it more understandable

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,7 @@
 import "bootstrap";
+// below, we only import the displayPrice function (and not the file)
+// to be able to import it, we also need to export it from the file where the function is built
 import {displayPrice} from "../components/product-price"
 
-
+// below, we must call the function (it is not sufficient just to import it)
 displayPrice()
