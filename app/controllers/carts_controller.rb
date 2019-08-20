654e4b79c192cb187e9f0240172243a1f43f00dc
caroline dupre <duprecar@gmail.com>
2019-08-20 09:20:27 +0800

    added favorites, need to change

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -17,29 +17,9 @@ class CartsController < ApplicationController
       prices << product.price
       @products << product
     end
-    # là j'appelle, pour chaque id, l'instance produit.
-    # Dans ce produit, je vais pouvoir trouver le photo.id et le frame_combo.id.
-    # DU coup je vais pouvoir accéder aux infos de la table photos et du frame_combo.
 
     @quantity = @products.length
     @total_price = prices.inject(0){|sum,x| sum + x }
-    raise
-
   end
-
-
-# Pour chaque cart_product
-  # 1. je dois récupérer les cart products
-  # 2. je dois en tirer le material et le dimension
-  # 3. Je dois récupérer le prix du combo
-  # 4. je dois récupérer l'image de la photo et le titre
-
-
-  # question: qui gère la fonction delete un cart_product?
-
-# Ensuite, pour faire mon total_cart:
-# 1. Je dois compter le nombre de cart_product
-# 2. Je dois additionner les prix
-
 
 end
