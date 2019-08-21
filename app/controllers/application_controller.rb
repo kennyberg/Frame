1e3530388b3067932ac4a84674e5009c88da3edd
kenny bergounhe <bergounhe.kenny.code@gmail.com>
2019-08-22 02:45:47 +0800

    bugs mentionned in the trello fixed: redirect on previous page after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :store_user_location!, if: :storable_location?
   before_action :authenticate_user!
+
+  private
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
+  end
 end
