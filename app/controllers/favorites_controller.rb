697036096c4e71ac84e4139a5223394db72bf1b3
kenny bergounhe <bergounhe.kenny.code@gmail.com>
2019-08-22 20:30:29 +0800

    bug on redefining the eventlistener on the news uploaded photos is fixed

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,23 +1,26 @@
 class FavoritesController < ApplicationController
   def create
-      if Photo.where(api_id: params[:api_id]).first
-        @photo = Photo.where(api_id: params[:api_id]).first
-      else
-        @photo = Photo.new
-        @photo.api_id = params[:api_id]
-        @photo.api_url = params[:url_id]
-        @photo.save
-      end
-   @favorite = Favorite.new(photo_id: @photo.id, user_id: current_user.id)
-   @favorites = Favorite.where(photo_id: @photo.id, user_id: current_user.id)
-   if @favorites.empty?
+    if Photo.where(api_id: params[:api_id]).first
+      @photo = Photo.where(api_id: params[:api_id]).first
+    else
+      @photo = Photo.new
+      @photo.api_id = params[:api_id]
+      @photo.api_url = params[:url_id]
+      @photo.save
+    end
+
+    @favorite = Favorite.new(photo_id: @photo.id, user_id: current_user.id)
+    @favorites = Favorite.where(photo_id: @photo.id, user_id: current_user.id)
+
+    if @favorites.empty?
       @favorite.save
-   else
-    @favorite = @favorites.first
-  end
-   respond_to do |format|
-     format.js
-   end
+    else
+      @favorite = @favorites.first
+    end
+
+    respond_to do |format|
+      format.js
+    end
   end
 
   def destroy
@@ -26,9 +29,9 @@ class FavoritesController < ApplicationController
     if request.referrer.include?("dashboard")
       redirect_to user_dashboard_path
     else
-     respond_to do |format|
-       format.js
-     end
-  end
+      respond_to do |format|
+        format.js
+      end
+    end
   end
 end
